668471ecdd1a1c2f8ec89f15f624230ddf4f1e86
Cleiber Rodrigues <cgsrjunior@inf.ufrgs.br>
2023-06-19 14:58:43 -0300

    Fixed new node func

--- tree.c
+++ tree.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
+#include "tree.h"
 
 struct Node* newNode(int data)
 {
     // Allocate memory for new node
-    struct Node* node = (struct node*) malloc(sizeof(struct node));
+    struct Node* node = (struct Node*) malloc(sizeof(struct Node));
  
     // Assign data to this node
-    node->data = data;
+    node->value = data;
  
     // Initialize left and
     // right children as NULL
-    node->left = NULL;
-    node->right = NULL;
+    node->left_child = NULL;
+    node->right_child = NULL;
     return (node);
 }
